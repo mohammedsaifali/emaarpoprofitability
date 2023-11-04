9df55ab889d531128d94c816df2260c473464f56
Mohammed Saif Ali <44836109+mohammedsaifali@users.noreply.github.com>
2023-11-04 12:23:02 +0400

    Update app.py with currency support

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ import streamlit as st
 import pandas as pd
 
 # Define a function to aggregate Amount_Sum and PurchaseAmount based on a list of invoice numbers
-def aggregate_values_based_on_invoices(df, invoice_list):
+def aggregate_values_based_on_invoices(df, invoice_list, currency, conversion_rates):
     # Filter the DataFrame based on the given list of invoice numbers
     filtered_df = df[df['InvNo'].isin(invoice_list)]
+    
+    # Convert amounts if currency is not OMR
+    conversion_rate = conversion_rates.get(currency, 1)  # Default to 1 if currency is OMR
+    amount_sum = (filtered_df['Amount'].sum()) * conversion_rate
+    purchase_amount_sum = (filtered_df['PurchaseAmount'].sum()) * conversion_rate
+    
     return pd.Series({
-        'Amount_Sum': filtered_df['Amount'].sum(),
-        'PurchaseAmount_Sum': filtered_df['PurchaseAmount'].sum()
+        'Amount_Sum': amount_sum,
+        'PurchaseAmount_Sum': purchase_amount_sum
     })
 
 # Define a function to fetch the unique 'BillTo' based on a list of invoice numbers
@@ -22,6 +28,17 @@ def process_sales_data(sales_register_file, sales_profitability_file):
     sales_register_df = pd.read_excel(sales_register_file, skiprows=3)
     sales_profitability_df = pd.read_excel(sales_profitability_file, skiprows=3)
 
+    # Define conversion rates
+    conversion_rates = {
+        'SAR': 0.1,
+        'QAR': 0.11,
+        'USD': 0.38
+        # Add other currencies and rates if necessary
+    }
+
+    # Determine the currency for each PONo
+    currency_per_po_df = sales_register_df.groupby('PONo')['Currency'].first().reset_index()
+
     re_aggregated_sales_profitability_df = sales_profitability_df.groupby(['InvNo', 'BillTo']).agg({
         'Amount': 'sum',
         'PurchaseAmount': 'sum'
@@ -35,11 +52,20 @@ def process_sales_data(sales_register_file, sales_profitability_file):
     # Rename columns for clarity
     aggregated_invoice_list_df.columns = ['PONo', 'InvoiceNos_List']
 
-    aggregated_values_df = aggregated_invoice_list_df.copy()
+    # Join the currency information with the aggregated invoice list
+    aggregated_values_df = aggregated_invoice_list_df.merge(currency_per_po_df, on='PONo', how='left')
+
+    # Modify the aggregation logic to include currency conversion
     aggregated_values_df['InvoiceNos_List'] = aggregated_values_df['InvoiceNos_List'].str.split(', ')
-    aggregated_values_df = aggregated_values_df.join(aggregated_values_df['InvoiceNos_List'].apply(
-        lambda x: aggregate_values_based_on_invoices(re_aggregated_sales_profitability_df, x)
-    ))
+    aggregated_values_df[['Amount_Sum', 'PurchaseAmount_Sum']] = aggregated_values_df.apply(
+        lambda x: aggregate_values_based_on_invoices(
+            re_aggregated_sales_profitability_df, 
+            x['InvoiceNos_List'], 
+            x['Currency'], 
+            conversion_rates
+        ), 
+        axis=1
+    )
 
     # Fetch the 'BillTo' for each list of invoice numbers
     aggregated_values_df['BillTo'] = aggregated_values_df['InvoiceNos_List'].apply(
